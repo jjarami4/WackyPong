f538efcecd30b115ee332edd1dcfc30bf0e22421
Joshua Jaramillo <42547328+jjarami4@users.noreply.github.com>
2018-08-28 23:36:34 -0600

    Add files via upload
    heeelllll yyeaaahhh

---
+++ scripts/gameplay/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A paddle
+/// </summary>
+public class Paddle : MonoBehaviour
+{
+    //variables
+    Rigidbody2D RB2D;
+	
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+        //attaches rigidbody to object
+        RB2D = gameObject.AddComponent<Rigidbody2D>();
+
+    }
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+		
+	}
+	
+}
